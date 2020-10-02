ffc4ae2e21af6ea96edfdb835ccb8e6e46c38b5e
GorelovaP <70329025+GorelovaP@users.noreply.github.com>
2020-10-02 14:48:37 +0300

    fix: new code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-
-exports.min = function min (array) {
+exports.min = function min (array)
+{
+  if (! array || !array.length) 
+  {
   return 0;
+  } else
+{ var min = array[0];
+for (var i = 0; i < array.length; i++) {
+    if (min > array[i]) 
+    min = array[i];
+}
+return min;
+}
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array) 
+{if (! array || !array.length) 
+    {
+    return 0;
+    } else
+{
+ var max = array[0]; // берем первый элемент массива
+    for (var i = 0; i < array.length; i++) { // переберем весь массив
+        // если элемент больше, чем в переменной, то присваиваем его значение переменной
+        if (max < array[i]) max = array[i]; 
+    }
+    // возвращаем максимальное значение
+    return max;
+}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) 
+{
+    if (! array || !array.length) 
+    {
+    return 0;
+    } else
+    {
+    var total = 0;
+    for (var i = 0; i < array.length; i++) 
+     {
+    total += array[i];
+     }
+    var avg = total / array.length;
+    return avg;
+    }
 }
